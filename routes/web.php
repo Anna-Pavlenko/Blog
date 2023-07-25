be76a4aa98b150a0cc058a02f9c067b29da4ca14
Anna-Pavlenko <annapavlenko637@gmail.com>
2023-07-25 14:58:24 +0300

    3 commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', 'IndexController')->name('index');
 });
+
 
 Auth::routes();
 
